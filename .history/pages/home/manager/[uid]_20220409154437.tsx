4e8773be5bf404e724a5adf1d1833a0c5e36186a
ren <pinokionigg@icloud.com>
2022-04-09 16:09:22 +0900

    release ver0.7

---
+++ .history/pages/home/manager/[uid]_20220409154437.tsx
@@ -0,0 +1,10 @@
+import HomePage_Manager from "../../../components/templates/Home/manager";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <HomePage_Manager />
+    </>
+  );
+}
